011f3653cacf8f942671b6ee3d4c8ecdad4bf772
qinman <qinman@soufunqinmandeimac.local>
2015-11-20 11:13:04 +0800

    Initial Commit

---
+++ UIAiertView点击区域外弹框消失/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UIAiertView点击区域外弹框消失
+//
+//  Created by qinman on 15/11/20.
+//  Copyright © 2015年 qinman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
