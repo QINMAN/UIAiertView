94a80f8ecf3401947448324428df11a0bf3ffca3
QINMAN <qm_199301@163.com>
2015-11-25 14:12:18 +0800

    first commit

---
+++ UIAiertView点击区域外弹框消失/类别/UIAlertView+Disappear.h
@@ -0,0 +1,13 @@
+//
+//  UIAlertView+Disappear.h
+//  UIAiertView点击区域外弹框消失
+//
+//  Created by qinman on 15/11/20.
+//  Copyright © 2015年 qinman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIAlertView (Disappear)
+
+@end
---
+++ UIAiertView点击区域外弹框消失/类别/UIAlertView+Disappear.m
@@ -0,0 +1,13 @@
+//
+//  UIAlertView+Disappear.m
+//  UIAiertView点击区域外弹框消失
+//
+//  Created by qinman on 15/11/20.
+//  Copyright © 2015年 qinman. All rights reserved.
+//
+
+#import "UIAlertView+Disappear.h"
+
+@implementation UIAlertView (Disappear)
+
+@end
