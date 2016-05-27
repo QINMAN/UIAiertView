e2454f7776f528325acf1f87342630ef61f7fa64
QINMAN <qm_199301@163.com>
2016-05-27 16:55:49 +0800

    UIAiertView点击区域外弹框消失

---
+++ UIAiertView点击区域外弹框消失/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIAiertView点击区域外弹框消失
+//
+//  Created by qinman on 15/11/20.
+//  Copyright © 2015年 qinman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UIAiertView点击区域外弹框消失/ViewController.m
@@ -0,0 +1,36 @@
+//
+//  ViewController.m
+//  UIAiertView点击区域外弹框消失
+//
+//  Created by qinman on 15/11/20.
+//  Copyright © 2015年 qinman. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "UIAlertView+Disappear.h"
+
+@interface ViewController ()<UIAlertViewDelegate>
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeContactAdd];
+    btn.frame = CGRectMake(10, 100, 100, 100);
+    [btn setTitle:@"click" forState:UIControlStateNormal];
+    [self.view addSubview:btn];
+    [btn addTarget:self action:@selector(makeAlert) forControlEvents:UIControlEventTouchUpInside];
+}
+
+- (void)makeAlert {
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:nil message:@"点击区域外弹框消失" delegate:self cancelButtonTitle:@"cancle" otherButtonTitles:@"confirm", nil];
+    [alert show];
+    [alert disappearAlertView];
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    NSLog(@"点击的是--%zd",buttonIndex);
+}
+@end
